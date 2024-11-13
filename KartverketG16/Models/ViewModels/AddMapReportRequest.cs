03ce767518bee289355a78f10562525dc1dd581d
jaeyu21 <144026410+jaeyu21@users.noreply.github.com>
2024-11-13 01:45:46 +0100

    changes

---
+++ KartverketG16/Models/ViewModels/AddMapReportRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KartverketG16.Models.ViewModels
+{
+	public class AddMapReportRequest
+	{
+		[Required(ErrorMessage = "A GeoJson shape is required. Please draw a shape on the map.")]
+		public string? GeoJson { get; set; }
+
+		[Required]
+		public string? Title { get; set; }
+
+		[Required]
+		public string? Description { get; set; }
+
+		[Required]
+		public int MapLayerId { get; set; }
+
+		public string? County { get; set; }
+
+		public string? Municipality { get; set; }
+
+		public List<IFormFile>? Attachments { get; set; } // Legg til dette feltet for vedleggsopplasting
+
+	}
+}
